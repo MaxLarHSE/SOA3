09712db1f706d60c7408f527fdb8b9979c0ab279
VaksHSE <malarionov@edu.hse.ru>
2026-05-02 15:54:14 +0300

    asdasd

--- booking-service/http/handler.go
+++ booking-service/http/handler.go
@@ -17,7 +17,7 @@ type Handler struct {
 	flightClient *client.FlightClient
 }
 
-// asdasd asdasdasd
+// asdasd asdasdasd asdasdasdasd
 func NewHandler(
 	bookingRepo *booking_service.BookingRepo,
 	flightClient *client.FlightClient,
